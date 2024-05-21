389c50838371b8e668bfd0ed802d402834f349b2
yalladev <yalladevofficial@gmail.com>
2024-05-21 12:02:39 +0300

    edit and redone

--- js/sumofpositive.js
+++
@@ -1,8 +0,0 @@
-// Subscribe to my channel :  https://www.youtube.com/@yalladev
-// Follow me on X (Twitter) : https://twitter.com/abdsymoh
-
-// Day 4 Of 100 Days Of JavaScript Problems Solving
-
-// (4) Sum of positive
-
-
